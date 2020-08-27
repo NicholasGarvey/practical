ba85a34e5a9d4c6b45daeb6725fb4f3c9b31d454
Nicholas Garvey <nicholas.garvey@jcu.edu.au>
2020-08-28 01:13:50 +1000

    add random function program

--- prac03/broken_score_functions.py
+++ prac03/broken_score_functions.py
@@ -1,6 +1,14 @@
+import random
+
+
 def main():
     score = float(input("Enter score: "))
     print(determine_status(score))
+    print(determine_random(r_number))
+
+
+r_number = random.randint(0, 100)
+print(r_number)
 
 
 def determine_status(score):
@@ -14,4 +22,13 @@ def determine_status(score):
         return "Bad"
 
 
+def determine_random(r_number):
+    if r_number < 0 or r_number > 100:
+        return "Invalid score"
+    elif r_number >= 90:
+        return "Excellent"
+    elif r_number >= 50:
+        return "Passable"
+    else:
+        return "Bad"
 main()
