397600ae24d43d1418ab85064bae6f622005389b
Nicholas Garvey <nicholas.garvey@jcu.edu.au>
2020-09-10 22:45:07 +1000

    updated prac05

---
+++ prac05/emails.py
@@ -0,0 +1,23 @@
+def main():
+    emails_name = {}
+    email = input("Email: ")
+    while email != "":
+        name = get_name_from_emails(email)
+        confirmation = input("Is your name {}? (Y/n) ".format(name))
+        if confirmation.upper() != "Y" and confirmation != "":
+            name = input("Name: ")
+        emails_name[email] = name
+        email = input("Email: ")
+
+    for email, name in emails_name.items():
+        print("{} ({})".format(name, email))
+
+
+def get_name_from_emails(email):
+    prefix = email.split('@')[0]
+    parts = prefix.split('.')
+    name = " ".join(parts).title()
+    return name
+
+
+main()
