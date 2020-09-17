9a504b283dea05aff6a60ac095f662f8bec90e97
Nicholas Garvey <nicholas.garvey@jcu.edu.au>
2020-09-17 23:29:04 +1000

    updated prac06

--- prac06/guitar_test.py
+++ prac06/guitar_test.py
@@ -11,17 +11,11 @@ def run_test():
     guitar = Guitar(name, year, cost)
     other = Guitar("Another Guitar", 2012, 1512.9)
 
-    print("{} get_age() - Expected {}. Got {}".format(guitar.name, 95,
-                                                      guitar.get_age()))
-    print("{} get_age() - Expected {}. Got {}".format(other.name, 5,
-                                                      other.get_age()))
+    print("{} get_age() - Expected {}. Got {}".format(guitar.name, 95, guitar.get_age()))
+    print("{} get_age() - Expected {}. Got {}".format(other.name, 5, other.get_age()))
     print()
-    print("{} is_vintage() - Expected {}. Got {}".format(guitar.name,
-                                                         True,
-                                                         guitar.is_vintage()))
-    print("{} is_vintage() - Expected {}. Got {}".format(other.name,
-                                                         False,
-                                                         other.is_vintage()))
+    print("{} is_vintage() - Expected {}. Got {}".format(guitar.name, True, guitar.is_vintage()))
+    print("{} is_vintage() - Expected {}. Got {}".format(other.name, False, other.is_vintage()))
 
 
 if __name__ == '__main__':
