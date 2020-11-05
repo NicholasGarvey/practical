8467ae8501951b620f04b44bcfdaee474db8604a
Nicholas Garvey <nicholas.garvey@jcu.edu.au>
2020-11-05 22:24:59 +1000

    updated work on prac10

--- prac01/hello.py
+++ prac01/hello.py
@@ -1,9 +1,10 @@
 # print("Hello world")
 def main():
-    print("Hello world")
-
-words = ["aye", "bee", "sea", "bee"]
-words.remove("bee")
-print(words.pop())
+    #print("Hello world")
+    result = print(" ".join([str(i * 2) for i in range(1, 16, 3) if i % 2 == 0]))
+    print(result)
+#words = ["aye", "bee", "sea", "bee"]
+#words.remove("bee")
+#print(words.pop())
 
 main()
